a462ad6be3c16c6fbb6773a6a0098dec95e027f3
Aleksandr Zinovev <uplink.std@gmail.com>
2022-03-22 23:27:45 +0300

    feature/pr-8 Реализация класса PopupWithForm

--- src/PopupWithForm.js
+++ src/PopupWithForm.js
@@ -1,3 +1,39 @@
+import { Popup } from "./Popup.js"
+
 class PopupWithForm extends Popup {
 
+  constructor(popupSelector, submitCallback) {
+    super(popupSelector);
+    this._submitCallback = submitCallback.bind(this);
+    this._setFormElements();
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._formElement.addEventListener('submit', this._submitCallback);
+  }
+
+  close() {
+    super.close();
+    this.reset();
+  }
+
+  reset() {
+    this._formElement.reset();
+  }
+
+  _getInputValues() {
+    const values = {};
+    this._inputList.forEach( (inputElement) => { values[inputElement.id] = inputElement.value; } )
+    return values;
+  }
+
+  _setFormElements() {
+    this._formElement = this._element.querySelector('.form');
+    this._submitButtonElement = this._formElement.querySelector('.form__save-btn');
+    this._inputList = Array.from(this._formElement.querySelectorAll('.form__input'));
+  }
+
 }
+
+export { PopupWithForm };
