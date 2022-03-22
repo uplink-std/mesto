bab3045a68cedfb2fa25482cf98077b81bcdde62
Aleksandr Zinovev <uplink.std@gmail.com>
2022-03-22 15:54:36 +0300

    feature/pr-8 Балванки классов

---
+++ src/Popup.js
@@ -0,0 +1,12 @@
+class Popup {
+
+  constructor(popupSelector) {
+    this._popupSelector = popupSelector;
+    this._setElements();
+  }
+
+  _setElements() {
+
+  }
+
+}
---
+++ src/PopupWithForm.js
@@ -0,0 +1,3 @@
+class PopupWithForm extends Popup {
+
+}
