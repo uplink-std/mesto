2b556426326e9268aa0cb75ec8efcd9acc6cece7
Aleksandr Zinovev <uplink.std@gmail.com>
2022-03-22 23:36:36 +0300

    feature/pr-8 Реализация класса UserInfo

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,2 +1,27 @@
 class UserInfo {
+
+  constructor({ nameSelector, occupationSelector }) {
+    this._nameSelector = nameSelector;
+    this._occupationSelector = occupationSelector;
+    this._setElements();
+  }
+
+  getUserInfo() {
+    return {
+      name: this._nameElement,
+      occupation: this._occupationElement
+    };
+  }
+
+  setUserInfo({ name, occupation }) {
+    this._nameElement.textContent = name;
+    this._occupationElement.textContent = occupation;
+  }
+
+  _setElements() {
+    this._nameElement = document.querySelector(this._nameSelector);
+    this._occupationElement = document.querySelector(this._occupationSelector);
+  }
 }
+
+export { UserInfo };
