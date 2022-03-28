3db0d73b0880376955cb541c1447b9fff07c51e5
Aleksandr Zinovev <uplink.std@gmail.com>
2022-03-28 18:46:47 +0300

    issue/pr-8 Лишняя переменная и лишний поиск в DOM. Искать нужный элемент нужно в попапе, который был найден в родителе.

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -15,9 +15,8 @@ class PopupWithImage extends Popup {
   }
 
   _setContainerElements() {
-    this._viewElement = document.querySelector('.popup_view-card');
-    this._imageElement = this._viewElement.querySelector('.card-details__photo');
-    this._nameElement = this._viewElement.querySelector('.card-details__name');
+    this._imageElement = this._element.querySelector('.card-details__photo');
+    this._nameElement = this._element.querySelector('.card-details__name');
   }
 
 }
