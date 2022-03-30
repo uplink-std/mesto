0f7b3470cca00ff0d0b7741291a30ccc59878f2e
Aleksandr Zinovev <uplink.std@gmail.com>
2022-03-30 17:54:42 +0300

    issue/pr-8 Константы лучше вынести в отдельный файл constants.js  в папке util

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import './index.css';
+import { popupSelectors, userInfoSelectors, validationOptions } from "../util/constants.js"
 import { Card } from "../components/Card.js";
 import { FormValidator } from "../components/FormValidator.js";
 import {PopupWithImage} from "../components/PopupWithImage";
@@ -6,30 +7,6 @@ import {PopupWithForm} from "../components/PopupWithForm";
 import {UserInfo} from "../components/UserInfo";
 import {Section} from "../components/Section";
 
-const popupSelectors = {
-  popupCloseButton: '.popup__close-btn',
-  popupOpenClass: 'popup_opened',
-  popupEditProfile: '.popup_edit-profile',
-  popupAddCard: '.popup_add-card',
-  popupViewCard: '.popup_view-card',
-  popupContainer: '.popup__container',
-}
-
-const userInfoSelectors = {
-  nameSelector: '.profile__info-name',
-  occupationSelector: '.profile__info-occupation',
-  editButton: '.profile__edit-btn',
-  addCardButton: '.profile__add-btn',
-};
-
-const validationOptions = {
-  formSelector: '.form',
-  inputSelector: '.form__input',
-  submitButtonSelector: '.form__save-btn',
-  inactiveButtonClass: 'form__save-btn_disabled',
-  inputErrorClass: 'form__input-error',
-  errorClass: 'form__input-error-msg_active'
-}
 
 const createFormValidator = (name) => {
   const validator = new FormValidator(validationOptions, document.forms[name]);
