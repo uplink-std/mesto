6e02bd4fbc5b348b7725684e9d06a2da12b69ee4
Aleksandr Zinovev <uplink.std@gmail.com>
2022-03-30 17:51:07 +0300

    issue/pr-8 Названия переменных должны соответствовать хранимым в них данным. Вы получаете на вход селектор, по которому должны найти элемент и его уже сохранить в поле класса. Приведите пожалуйста названия в соответствие содержимому.

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,9 +1,8 @@
 class UserInfo {
 
   constructor({ nameSelector, occupationSelector }) {
-    this._nameSelector = nameSelector;
-    this._occupationSelector = occupationSelector;
-    this._setElements();
+    this._nameElement = document.querySelector(nameSelector);
+    this._occupationElement = document.querySelector(occupationSelector);
   }
 
   getUserInfo() {
@@ -16,11 +15,6 @@ class UserInfo {
   setUserInfo({ name, occupation }) {
     this._nameElement.textContent = name;
     this._occupationElement.textContent = occupation;
-  }
-
-  _setElements() {
-    this._nameElement = document.querySelector(this._nameSelector);
-    this._occupationElement = document.querySelector(this._occupationSelector);
   }
 }
 
